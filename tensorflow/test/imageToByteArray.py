cd53aa515a3732ce8641b297403c3a267240fef3
Coty Embry <cotyembry@gmail.com>
2017-06-09 22:05:03 -0500

    added logic to give an example of how to convert an image into a ubyte array, convert that array into binary, then write the binary data out to a file

--- tensorflow/test/imageToByteArray.py
+++ tensorflow/test/imageToByteArray.py
@@ -1,18 +1,24 @@
-import numpy as np
+#Author:		John Coty Embry
+#Date Created:	06-09-2017
+#Last Modified:	06-09-2017
 
+import numpy as np
+import struct
+
+
+#1. convert the image into a byte array
 with open("img.png", "rb") as imageFile:
   f = imageFile.read()
   b = bytearray(f)
 
-# print('b = ', b)
 
-# this works
-for i in range(len(b)):
-	if(i < 10):
-		print('byte = ', b[i])
+# print('type = ', type(b[0])) #to see its type
 
-bytestream = np.fromfile('img.png', dtype=np.uint8)
+#2. convert the array of ubytes into binary data (this was from stack overflow)
+binaryData = ''.join(map(chr,b))
 
-for i in range(len(bytestream)):
-	if(i < 10):
-		print('ubyte = ', bytestream[i])
+#3. get a new file ready to be written to
+newFile = open('./newFileNameToGive.ubyte', 'wb') #w I assume means 'write' but I dont know what b means
+
+#4. actually write the binary to a file
+newFile.write(binaryData) #this writes out the file as binary data
